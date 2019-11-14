6396fe83f10f8aa2b61282a565f45ccc186820c8
neekey <ni184775761@gmail.com>
2019-11-14 11:32:40 +1100

    remove&add style

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import style from './app.scss';
 
 export default function App() {
   return (
-    <div>Hello React world</div>
+    <div className={style.app}>Hello React world</div>
   );
 }
